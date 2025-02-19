9760db8c45b5881c36d6b3a6c3491623a334a2cf
JlimL <jeongliml2002@gmail.com>
2025-02-19 16:00:49 +0900

    header file for static board data structure

---
+++ src/static_controller/static_controller.h
@@ -0,0 +1,41 @@
+enum hard_ware 
+{
+  rfid
+  led_r,
+  led_g,
+  led_b,
+  led_w
+}
+
+enum board
+{
+  User_PC,
+  Desk_PC,
+  Dynamic,
+  Static
+}
+
+struct control_data{
+  byte function_code,
+  byte mode,
+  byte led_r,
+  byte led_g,
+  byte led_b,
+  byte led_w,
+  byte servo1,
+  byte servo2,
+  byte linear_actuator_upper,
+  byte linear_actuator_lower
+}
+
+struct rfid_data{
+  byte mode,
+  byte led_r,
+  byte led_g,
+  byte led_b,
+  byte led_w,
+  byte servo1,
+  byte servo2,
+  byte linear_actuator_upper,
+  byte linear_actuator_lower
+}
